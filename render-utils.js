2e597412d8f86d35aa01a07f7663eec34177793a
Akilah Powell <atpjun@gmail.com>
2022-11-28 21:41:09 -0400

    fetch utils create todo

--- render-utils.js
+++ render-utils.js
@@ -1,4 +1,15 @@
 export function renderTodo(todo, handleComplete) {
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+
+    div.classList.add(todo.complete ? 'complete' : 'incomplete');
+    div.classList.add('todo');
+
+    p.textContent = todo.todo;
+
+    div.append(p);
+
+    return div;
     // create a div and a p tag
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
 
@@ -9,6 +20,6 @@ export function renderTodo(todo, handleComplete) {
     // append stuff
 
     // add event listener for click and call handleComplete function
-    
+
     // return the div
 }
